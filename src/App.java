1dbc79376e3edd83aab43dccc9c162922081b6ff
Fredrik Dellby <dellby.fredrik@gmail.com>
2021-04-07 13:20:44 +0200

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,43 @@
+public class App {
+    private static int counter = 0;
+
+    public static synchronized void increment() {
+        ++counter;
+    }
+
+    public static void process() {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; ++i) {
+                    increment();
+                    System.out.println("Thread 1");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; ++i) {
+                    increment();
+                    System.out.println("Thread 2");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        process();
+    }
+}
