aabc8a12329cabbf1b4eaece67a6bb615207a28b
FredrikDellby <dellby.fredrik@gmail.com>
2021-04-12 17:50:18 +0200

    Add files via upload
    Adding AtomicIntegerDemo.

---
+++ src/AtomicIntegerDemo.java
@@ -0,0 +1,28 @@
+package com.balazsholczer.udemy;
+
+import java.util.concurrent.atomic.*;
+
+public class AtomicIntegerDemo {
+	private static AtomicInteger counter = new AtomicInteger(0);
+
+	public static void main(String[] args) {
+		Thread t1 = new Thread(AtomicIntegerDemo::increment, "thread1");
+		Thread t2 = new Thread(AtomicIntegerDemo::increment, "thread2");
+		
+		t1.start(); t2.start();
+		
+		try {
+			t1.join(); t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Counter: " + counter);
+	}
+	
+	public static synchronized void increment() {
+		for (int i=0; i<10000; i++)
+			counter.getAndIncrement();
+	}
+
+}
